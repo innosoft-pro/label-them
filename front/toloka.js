f3d94d408d3b38edeaf2fe6e893b0a512f5095e1
Alex M <alexgotfrag@gmail.com>
2017-03-09 11:04:28 +0300

    Add event listeners to buttons in toloka

---
+++ front/toloka.js
@@ -0,0 +1,48 @@
+var methodsArray = [save, selectLabel, panTool];
+
+function save() {
+    alert('Impement save');
+}
+
+function panTool() {
+    alert('Implement pan tool');
+}
+
+function selectLabel() {
+    alert('Implement select label');
+}
+
+exports.Task = extend(TolokaHandlebarsTask, function (options) {
+    TolokaHandlebarsTask.call(this, options);
+}, {
+
+    /**
+     *  Add listener to each button based on its class on start
+     *  appropriate class for each button should be added in html
+     */
+    onRender: function () {
+        methodsArray.forEach(method => {
+            this.getDOMElement()
+                .getElementsByClassName(method.name)[0]
+                .addEventListener('click', method);
+        });
+
+    },
+    onDestroy: function () {
+        //
+
+    }
+});
+
+function extend(ParentClass, constructorFunction, prototypeHash) {
+    constructorFunction = constructorFunction || function () {
+        };
+    prototypeHash = prototypeHash || {};
+    if (ParentClass) {
+        constructorFunction.prototype = Object.create(ParentClass.prototype);
+    }
+    for (var i in prototypeHash) {
+        constructorFunction.prototype[i] = prototypeHash[i];
+    }
+    return constructorFunction;
+}
