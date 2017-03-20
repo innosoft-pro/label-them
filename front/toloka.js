6fe9aa06bcdb05cd0360d92bbaac1fb1924ef1f6
Aleksandr Nedorezov <Al73rus@gmail.com>
2017-03-20 15:43:55 +0300

    LT-65 Modified toloka.js & main.html - requesting data from json works

--- front/toloka.js
+++ front/toloka.js
@@ -1,4 +1,4 @@
-var methodsArray = [save, selectLabel, panTool];
+var methodsArray = [save, selectLabel, panTool, getJSON];
 
 function save() {
     alert("Impement save");
@@ -12,28 +12,31 @@ function selectLabel() {
     alert("Implement select label");
 }
 
+function render(dom) {
+    methodsArray.forEach(method = > {
+        console.log(method.name)
+    dom
+        .getElementsByClassName(method.name)[0]
+        .addEventListener('click', method);
+})
+    ;
+}
+
 // Uncomment before the deployment in Yandex.Toloka
 /*
-exports.Task = extend(TolokaHandlebarsTask, function (options) {
-    TolokaHandlebarsTask.call(this, options);
-}, {
+ exports.Task = extend(TolokaHandlebarsTask, function (options) {
+ TolokaHandlebarsTask.call(this, options);
+ }, {
 
-    // Add listener to each button based on its class on start
-    // appropriate class for each button should be added in html
-    onRender: function () {
-        methodsArray.forEach(method => {
-            this.getDOMElement()
-                .getElementsByClassName(method.name)[0]
-                .addEventListener('click', method);
-        });
+ // Add listener to each button based on its class on start
+ // appropriate class for each button should be added in html
+ onRender: function() { render(this.getDOMElement()); },
+ onDestroy: function () {
+ //
 
-    },
-    onDestroy: function () {
-        //
-
-    }
-});
-*/
+ }
+ });
+ */
 
 function extend(ParentClass, constructorFunction, prototypeHash) {
     constructorFunction = constructorFunction || function () {
@@ -46,4 +49,29 @@ function extend(ParentClass, constructorFunction, prototypeHash) {
         constructorFunction.prototype[i] = prototypeHash[i];
     }
     return constructorFunction;
+}
+
+// request data from JSON file
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'https://rawgit.com/innosoft-pro/label-them/4ecbd548ae1315469a73118e8c807c451ee95a56/front/classesandparameters.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+
+        }
+    };
+    xobj.send(null);
+}
+
+function getJSON() {
+    // Call to function with anonymous callback
+    loadJSON(function (response) {
+        jsonresponse = JSON.parse(response);
+
+        alert(jsonresponse.result);
+    });
 }
