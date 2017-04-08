1526b56116794ddcbf32e4c15b85e00f8ca3aef7
Aleksandr Nedorezov <Al73rus@gmail.com>
2017-04-08 21:00:04 +0300

    LT-125 modified generateHTMLCodeForClassesAndParameters method in front/js/toloka.js to work correctly with modified classesandparameters.json

--- front/js/toloka.js
+++ front/js/toloka.js
@@ -1,5 +1,6 @@
 var urlToJSONWithClassesAndParameters =
-    "https://rawgit.com/innosoft-pro/label-them/develop/front/json/classesandparameters.json";
+    "https://rawgit.com/innosoft-pro/label-them/LT-125_modify_classesandparameters.json/" +
+    "front/json/classesandparameters.json";
 
 function save() {
     alert("Implement save");
@@ -74,22 +75,18 @@ function generateHTMLCodeForClassesAndParameters(dom) {
 
         var html = [];
 
-        var generalClassesNameTemplate = [];
         var classes = [];
         var parameters = [];
 
-        generalClassesNameTemplate.push("<h4>");
-        generalClassesNameTemplate.push(jsonresponse.generalClassesName);
-        generalClassesNameTemplate.push(" Type");
-        generalClassesNameTemplate.push("</h4>");
-        generalClassesNameTemplate = generalClassesNameTemplate.join("");
+        classes.push("<h4>");
+        classes.push("Objects Classes");
+        classes.push("</h4>");
 
         classes.push("<div class=\"dropdown\">");
         classes.push("<button class=\"btn btn-default dropdown-toggle\" type=\"button\" ");
         classes.push("id=\"dropdownMenu-Classes\" ");
         classes.push("data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">");
-        classes.push(jsonresponse.generalClassesName);
-        classes.push(" Type");
+        classes.push("Objects Classes");
         classes.push("<span class=\"caret\"></span>");
         classes.push("</button>");
         classes.push("<ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu-Classes\">");
@@ -109,19 +106,19 @@ function generateHTMLCodeForClassesAndParameters(dom) {
             var inputGroupsCount = 0;
             parameters.push("<form>");
             jsonresponse.parameters.forEach(function (obj) {
-                if (obj.type === "checkbox") {
+                if (obj.type === "boolean") {
                     parameters.push("<div class=\"checkbox\">");
                     parameters.push("<label><input type=\"checkbox\" value=\"\">");
                     parameters.push(obj.name);
                     parameters.push("</label>");
                     parameters.push("</div>");
-                } else if (obj.type === "input-group") {
+                } else if (obj.type === "string") {
                     inputGroupsCount++;
                     parameters.push("<div class=\"input-group\">");
                     parameters.push("<span class=\"input-group-addon\" id=\"basic-addon");
                     parameters.push(inputGroupsCount);
                     parameters.push("\">");
-                    parameters.push(obj.addon);
+                    parameters.push(obj.prefix);
                     parameters.push("</span>");
                     parameters.push("<input type=\"text\" class=\"form-control\" placeholder=\"");
                     parameters.push(obj.name);
@@ -129,13 +126,16 @@ function generateHTMLCodeForClassesAndParameters(dom) {
                     parameters.push(inputGroupsCount);
                     parameters.push("\">");
                     parameters.push("</div>");
-                } else if (obj.type === "dropdown-menu") {
+                } else if (obj.type === "select") {
                     dropdownMenusCount++;
-                    parameters.push("<h4>");
-                    parameters.push(obj.name);
-                    parameters.push("</h4>");
                     parameters.push("<div class=\"dropdown\">");
-                    parameters.push("<button class=\"btn btn-default dropdown-toggle\" type=\"button\" ");
+                    parameters.push("<button class=\"btn btn-default dropdown-toggle");
+                    // Additional margin to add some space between input groups and drop down menus
+                    // Otherwise they stick to one another
+                    if (inputGroupsCount > 0 && dropdownMenusCount === 1 || dropdownMenusCount > 1) {
+                        parameters.push(" dropdown-menu-parameters");
+                    }
+                    parameters.push("\" type=\"button\" ");
                     parameters.push("id=\"dropdownMenu-parameters");
                     parameters.push(dropdownMenusCount);
                     parameters.push("\" ");
@@ -147,7 +147,7 @@ function generateHTMLCodeForClassesAndParameters(dom) {
                     parameters.push(dropdownMenusCount);
                     parameters.push("\">");
 
-                    obj.menuItems.forEach(function (obj2) {
+                    obj.options.forEach(function (obj2) {
                         parameters.push("<li><a role=\"menuitem\" tabindex=\"-1\" href=\"#\">");
                         parameters.push(obj2);
                         parameters.push("</a></li>");
@@ -161,7 +161,6 @@ function generateHTMLCodeForClassesAndParameters(dom) {
             parameters = parameters.join("");
         }
 
-        html.push(generalClassesNameTemplate);
         html.push(classes);
         html.push(parameters);
 
