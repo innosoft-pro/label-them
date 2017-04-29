10d7711f00571b0baa4686e2d48225464ae6491e
aykbadalyan <ayk.badalyan@gmail.com>
2017-04-30 02:03:31 +0300

    [MF] getElementById exchanged with className.

---
+++ front/js/app/toloka.js
@@ -0,0 +1,64 @@
+// Uncomment before the deployment in Yandex.Toloka
+/*
+ exports.Task = extend(TolokaHandlebarsTask, function (options) {
+ TolokaHandlebarsTask.call(this, options);
+ }, {
+
+ // Add listener to each button based on its class on start
+ // appropriate class for each button should be added in html
+ onRender: function() { render(this.getDOMElement()); },
+ onDestroy: function () {
+ //
+
+ }
+ });
+ */
+
+function extend(ParentClass, constructorFunction, prototypeHash) {
+    constructorFunction = constructorFunction || function () {
+        };
+    prototypeHash = prototypeHash || {};
+    if (ParentClass) {
+        constructorFunction.prototype = Object.create(ParentClass.prototype);
+    }
+    for (var i in prototypeHash) {
+        constructorFunction.prototype[i] = prototypeHash[i];
+    }
+    return constructorFunction;
+}
+
+function initToloka() {
+    TolokaTask.prototype['onRender'] = function () {
+        alert("OnRender Called!");
+        console.log(this);
+        this.getDOMElement()
+            .getElementsByClassName("btnsave")[0]
+            .addEventListener('click', OnSave);
+
+        window.thisTask = this;
+
+        this.getDOMElement()
+            .getElementsByClassName("btn-polygon")[0]
+            .addEventListener('click', svgImgOnClick(event));
+
+    }
+}
+
+function tolokaReadyFn(jQuery) {
+    initToloka();
+    initDOM();
+    initCanvas();
+}
+
+$(document).ready(tolokaReadyFn);
+
+
+// var LTTask = extend(TolokaTask, function() {
+//     TolokaTask.call(this);
+// }, {
+//     onRender: function() {
+//       alert("OnRender Called!");
+//
+//
+//     }
+// })
