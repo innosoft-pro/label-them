8a5687e78f19d2bbcf4619e9cb76f67f2030c1e8
Ruslan <r.mustafin93@gmail.com>
2017-04-30 18:41:09 +0300

    added script to generate TSV

---
+++ utils/generate_tsv.py
@@ -0,0 +1,35 @@
+import csv
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('-d', '--data', help='Path to the dataset', required=True)
+parser.add_argument('-p', '--params', help='Path to the params JSON', required=True)
+parser.add_argument('-i', '--toloka_uid', help='Toloka Disk UID', required=True)
+
+args = vars(parser.parse_args())
+
+data_path = args['data']
+params_path = args['params']
+uid = args['toloka_uid']
+
+# print (data_path)
+# print (params_path)
+
+
+
+with open('tasks.tsv', 'w') as csvfile, open(params_path, 'r') as json_params:
+
+    json_str_proc = (''.join([l.strip() for l in json_params.readlines()])).replace('"', '\\')
+
+    fieldnames = ['INPUT:image_rel', 'INPUT:json_params', 'GOLDEN:result']
+    writer = csv.DictWriter(csvfile, delimiter='\t',fieldnames=fieldnames)
+
+    # Go through each file in a directory
+    for filename in os.listdir(data_path):
+        print(filename)
+
+        writer.writeheader()
+        writer.writerow({fieldnames[0]: '/{}/{}'.format(uid, filename), fieldnames[1]: json_str_proc, fieldnames[2]: 'true'})
