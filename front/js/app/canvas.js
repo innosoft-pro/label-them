f9cc79990acf4257832cbde532622a981f2bd953
Aleksandr Nedorezov <Al73rus@gmail.com>
2017-05-01 02:37:57 +0300

    LT-185 Cleaned up and commented code in canvas.js

--- front/js/app/canvas.js
+++ front/js/app/canvas.js
@@ -2,8 +2,8 @@ function initCanvas() {
 
     img.onload = function () {
        aspectRatio = img.width / img.height;
-       resizeSupportingOneToOneMapping();
-       window.addEventListener("resize", resizeSupportingOneToOneMapping, false);
+        resize();
+       window.addEventListener("resize", resize, false);
     }
 
     // node = document.getElementById('img_url');
@@ -25,41 +25,12 @@ function drawImg(img) {
     var canvas = document.getElementById("main-canvas");
     var ctx = canvas.getContext("2d");
 
-    var targetWidth = canvas.height * aspectRatio;
-    var targetX = (canvas.width - targetWidth) / 2;
-
-    ctx.drawImage(img, 0, 0, img.width, img.height,     // source rectangle
-        targetX, 0, targetWidth, canvas.height); // destination rectangle
-}
-
-// handles resizing of the canvas
-function resize() {
-    var canvas = document.getElementById("main-canvas");
-    var ctx = canvas.getContext("2d");
-
-    // make canvas fit parent div
-    var parent = document.getElementById("canvas-parent");
-    var svg = document.getElementById("svg_img");
-
-    // 28 is the width of two 14px paddings from each side of the canvas, specified in bootstrap
-    parent.style.minHeight = (parent.clientWidth + 28) / aspectRatio + "px";
-
-    var height = parent.clientHeight;
-    var width = parent.clientWidth;
-
-    // modify both canvas style and canvas dimension
-    canvas.style.height = height + "px";
-    canvas.style.width = width + "px";
-    canvas.width = width;
-    canvas.height = height;
-    svg.style.width = width;
-    svg.style.height = height;
-
-    drawImg(img);
+    ctx.drawImage(img, 0, 0, canvas.width, img.height,     // source rectangle
+        0, 0, canvas.width, canvas.height); // destination rectangle
 }
 
 // handles resizing of the canvas for 1 to 1 mapping between image height and canvas height
-function resizeSupportingOneToOneMapping() {
+function resize() {
     var canvas = document.getElementById("main-canvas");
     var ctx = canvas.getContext("2d");
 
@@ -73,6 +44,8 @@ function resizeSupportingOneToOneMapping() {
     var height = parent.clientHeight;
     var width = parent.clientWidth;
 
+    // resizing canvas if image width is smaller than the width of the block for the image
+    // to avoid redundant markup over empty space in the block
     if(width > img.width) {
         width = img.width;
     }
@@ -85,24 +58,17 @@ function resizeSupportingOneToOneMapping() {
     svg.style.width = width;
     svg.style.height = height;
 
+    // Notify the user that not the full image will be shown
     if(canvas.width < img.width) {
-            showMessage(
-                "Image cannot fit the screen in width. " +
-                "Only left part of the image that fits the screen is displayed! " +
-                "Please, markup the visible part of the image displayed below using the " +
-                "tools from the block on the left or skip this image.",
-                MessageTypeEnum.DANGER);
+        showMessage(
+            "Image cannot fit the screen in width. " +
+            "Only left part of the image that fits the screen is displayed! " +
+            "Please, markup the visible part of the image displayed below using the " +
+            "tools from the block on the left or skip this image.",
+            MessageTypeEnum.DANGER);
     }
 
-    drawImgSupportingOneToOneMapping(img);
-}
-
-function drawImgSupportingOneToOneMapping(img) {
-    var canvas = document.getElementById("main-canvas");
-    var ctx = canvas.getContext("2d");
-
-    ctx.drawImage(img, 0, 0, canvas.width, img.height,     // source rectangle
-        0, 0, canvas.width, canvas.height); // destination rectangle
+    drawImg(img);
 }
 
 // function CanvasReadyFn( jQuery ) {
