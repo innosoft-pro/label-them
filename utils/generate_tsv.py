2d4f803fb54ee526e402616154b746916279939f
Ruslan Mustafin <r.mustafin93@gmail.com>
2017-05-24 10:41:41 +0300

    fixed silent generation of the wrong TSV data given an incorret JSON with params (see #4)

--- utils/generate_tsv.py
+++ utils/generate_tsv.py
@@ -1,6 +1,16 @@
 import csv
 import argparse
 import os
+import json
+
+
+
+def json_valid(json_string):
+  try:
+    json_object = json.loads(json_string)
+  except ValueError, e:
+    return False
+  return True
 
 parser = argparse.ArgumentParser()
 
@@ -15,20 +25,20 @@ data_path = args['data']
 params_path = args['params']
 uid = args['toloka_uid']
 
-# print (data_path)
-# print (params_path)
-
-
-
 with open('tasks.tsv', 'w') as csvfile, open(params_path, 'r') as json_params:
 
-    json_str_proc = (''.join([l.strip() for l in json_params.readlines()])).replace('"', '\\')
+    json_string = ''.join(json_params.readlines())
 
-    fieldnames = ['INPUT:image_rel', 'INPUT:json_params', 'GOLDEN:result']
-    writer = csv.DictWriter(csvfile, delimiter='\t',fieldnames=fieldnames)
+    if json_valid(json_string):
+        json_str_proc = (''.join([l.strip() for l in json_params.readlines()])).replace('"', '\\')
 
-    writer.writeheader()
+        fieldnames = ['INPUT:image_rel', 'INPUT:json_params', 'GOLDEN:result']
+        writer = csv.DictWriter(csvfile, delimiter='\t',fieldnames=fieldnames)
 
-    # Go through each file in a directory
-    for filename in os.listdir(data_path):
-        writer.writerow({fieldnames[0]: '/{}/{}'.format(uid, filename), fieldnames[1]: json_str_proc, fieldnames[2]: 'true'})
+        writer.writeheader()
+
+        # Go through each file in a directory
+        for filename in os.listdir(data_path):
+            writer.writerow({fieldnames[0]: '/{}/{}'.format(uid, filename), fieldnames[1]: json_str_proc, fieldnames[2]: 'true'})
+    else:
+        raise ValueError('json with classes and params at {} has invalid format'.format(params_path))
