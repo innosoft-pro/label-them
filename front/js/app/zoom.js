d8e3fe48d8f7a0ef258269dcc07029c5ce468bfb
aykbadalyan <ayk.badalyan@gmail.com>
2017-06-14 21:46:11 +0300

    [MF] SVG and Canvas layers simultaneous zoom-in/zoom out.

--- front/js/app/zoom.js
+++ front/js/app/zoom.js
@@ -1,19 +1,26 @@
 var canvas;
+var svg;
+var default_canvas_width;
 
 function initZoom() {
     canvas = document.getElementById("main-canvas");
+    svg = document.getElementById("svg_img");
     default_canvas_width = canvas.width;
 }
 
 function zoomPlus() {
     canvas.width = canvas.width * 2;
     canvas.height = canvas.height * 2;
+    svg.style.width = svg.style.width.replace("px", "") * 2;
+    svg.style.height = svg.style.height.replace("px", "") * 2;
     drawImg(img);
 }
 
 function zoomMinus() {
-    if (canvas.width == default_canvas_width) return;
+    if (canvas.width === default_canvas_width) return;
     canvas.width = canvas.width / 2;
     canvas.height = canvas.height / 2;
+    svg.style.width = svg.style.width.replace("px", "") / 2;
+    svg.style.height = svg.style.height.replace("px", "") / 2;
     drawImg(img);
 }
