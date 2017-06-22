6101f33ccc1e2e4a25f5b2e77bc5356b716e4612
Alex M <alexgotfrag@gmail.com>
2017-06-22 13:29:33 +0300

    Automated brightness test

---
+++ test/specs/BrightnessTest.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+describe('webdriver.io page', function () {
+    it("Should change image brightness from 100 to 80", function () {
+        browser.url('http://localhost:63342/label-them/front/main_local.html');
+        browser.click("#btn_brightness_low");
+
+        var result = browser.execute("return defaultBrightness;")
+
+        assert.equal(result.value, 90);
+    });
+
+    it("Should check that brightness cannot be less than 10", function () {
+        browser.url('http://localhost:63342/label-them/front/main_local.html');
+        for (var i = 0; i < 11; i++) {
+            browser.click("#btn_brightness_low");
+        }
+
+        var result = browser.execute("return defaultBrightness;")
+
+        assert.equal(result.value, 10);
+    });
+
+    it("Should check that brightness cannot be more than 500", function () {
+        browser.url('http://localhost:63342/label-them/front/main_local.html');
+        for (var i = 0; i < 41; i++) {
+            browser.click("#btn_brightness_high");
+        }
+
+        var result = browser.execute("return defaultBrightness;")
+
+        assert.equal(result.value, 500);
+    });
+}); 
