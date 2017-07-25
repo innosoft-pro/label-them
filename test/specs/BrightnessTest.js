a94bd1b4588310efa1b28c03d128170b60eb07fb
Alex M <alexgotfrag@gmail.com>
2017-07-25 16:37:12 +0300

    Static analysis warnings fixed in the brightness test

--- test/specs/BrightnessTest.js
+++ test/specs/BrightnessTest.js
@@ -1,6 +1,6 @@
 let assert = require("assert");
 let projectFolderName = "label-them";
-describe('webdriver.io page', function () {
+describe("webdriver.io page", function () {
     it("Should change image brightness from 100 to 90", function () {
         /*global browser*/
         /*eslint no-undef: "error"*/
@@ -12,7 +12,7 @@ describe('webdriver.io page', function () {
     });
 
     it("Should change image brightness from 100 to 110", function () {
-        browser.url('http://localhost:63342/' + projectFolderName + '/front/main_local.html');
+        browser.url("http://localhost:63342/" + projectFolderName + "/front/main_local.html");
         browser.click("#btn_brightness_high");
 
         let result = browser.execute("return defaultBrightness;");
@@ -20,7 +20,7 @@ describe('webdriver.io page', function () {
     });
 
     it("Should check that brightness cannot be less than 10", function () {
-        browser.url('http://localhost:63342/' + projectFolderName + '/front/main_local.html');
+        browser.url("http://localhost:63342/" + projectFolderName + "/front/main_local.html");
         for (let i = 0; i < 11; i++) {
             browser.click("#btn_brightness_low");
         }
@@ -30,7 +30,7 @@ describe('webdriver.io page', function () {
     });
 
     it("Should check that brightness cannot be more than 500", function () {
-        browser.url('http://localhost:63342/' + projectFolderName + '/front/main_local.html');
+        browser.url("http://localhost:63342/" + projectFolderName + "/front/main_local.html");
         for (let i = 0; i < 41; i++) {
             browser.click("#btn_brightness_high");
         }
